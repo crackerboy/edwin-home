560d33436f435b46a3c29bffb48a537baf165434
estevez <vyalov.egor@gmail.com>
2018-08-12 23:58:20 +0300

    Fix possible connections issue

--- firmware/sensor_temp_humidity_mqtt/sensor_temp_humidity_mqtt.ino
+++ firmware/sensor_temp_humidity_mqtt/sensor_temp_humidity_mqtt.ino
@@ -1,26 +1,24 @@
 //WiFi enabled MQTT temperature and humidity sensor based on Wemos D1 mini and STH30 shield.
-//v.1.1
+//v.1.2
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <WEMOS_SHT3X.h>
 
 #define WIFI_SSID           "SSID"
-#define WIFI_PASSWORD       "WiFi password"
+#define WIFI_PASSWORD       "wifi_password"
 
 #define MQTT_SERVER         "192.168.2.197"
-#define MQTT_CLIENT_NAME    "d1mini01"
-#define MQTT_TOPIC_TEMP     "sensor/d1mini01/temperature"
-#define MQTT_TOPIC_HUMID    "sensor/d1mini01/humidity"
+#define MQTT_CLIENT_NAME    "ths001"
+#define MQTT_TOPIC_TEMP     "edwin/temphumisensor/temperature"
+#define MQTT_TOPIC_HUMID    "edwin/temphumisensor/humidity"
+#define MQTT_USER           "user"
+#define MQTT_PASSWORD       "password"
 
-#define PUBLISH_RATE        10*60      // deep sleep timer in seconds
+#define PUBLISH_RATE        10*60
 
-#define DEBUG               false      // debug to serial port
+#define DEBUG               false
 
-const char* mqtt_user = "user";
-const char* mqtt_password = "password";
-
-//Libraries initialization
 WiFiClient    wifi;
 PubSubClient  mqtt(MQTT_SERVER, 1883, wifi);
 SHT3X         sht30(0x45);
@@ -45,7 +43,7 @@ bool connectMQTT() {
   int retryCounter = 0;
     while ( !mqtt.connected() ) {
       if (DEBUG) Serial.println("connecting MQTT");
-      if ( !mqtt.connect(MQTT_CLIENT_NAME,mqtt_user,mqtt_password) ) {
+      if ( !mqtt.connect(MQTT_CLIENT_NAME, MQTT_USER, MQTT_PASSWORD) ) {
         retryCounter++;
         //Trying to connect 3 times
         if (retryCounter > 3) {
@@ -95,8 +93,8 @@ void setup() {
 }
 
 void goToSleep() {
-  if (WiFi.status() == WL_CONNECTED) WiFi.disconnect();
   if (mqtt.connected()) mqtt.disconnect();
+  if (WiFi.status() == WL_CONNECTED) WiFi.disconnect();
   ESP.deepSleep(PUBLISH_RATE * 1e6);  
 }
 
