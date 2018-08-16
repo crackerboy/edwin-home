6bd41e916f2d72144b7ee92f57d0c397a51bb1e3
estevz <vyalov.egor@gmail.com>
2018-08-16 09:57:24 +0300

    Add wifi client stop. Add WiFi operation mode setup.

--- firmware/sensor_temp_humidity_mqtt/sensor_temp_humidity_mqtt.ino
+++ firmware/sensor_temp_humidity_mqtt/sensor_temp_humidity_mqtt.ino
@@ -1,23 +1,21 @@
 //WiFi enabled MQTT temperature and humidity sensor based on Wemos D1 mini and STH30 shield.
-//v.1.2
+//v.1.3
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 #include <WEMOS_SHT3X.h>
 
-#define WIFI_SSID           "SSID"
-#define WIFI_PASSWORD       "wifi_password"
+#define WIFI_SSID           "*****"
+#define WIFI_PASSWORD       "************"
 
 #define MQTT_SERVER         "192.168.2.197"
-#define MQTT_CLIENT_NAME    "ths001"
+#define MQTT_CLIENT_NAME    "edwin-ths.1.3"
 #define MQTT_TOPIC_TEMP     "edwin/temphumisensor/temperature"
 #define MQTT_TOPIC_HUMID    "edwin/temphumisensor/humidity"
-#define MQTT_USER           "user"
-#define MQTT_PASSWORD       "password"
+#define MQTT_USER           "*********"
+#define MQTT_PASSWORD       "********"
 
 #define PUBLISH_RATE        10*60
-
-#define DEBUG               false
 
 WiFiClient    wifi;
 PubSubClient  mqtt(MQTT_SERVER, 1883, wifi);
@@ -25,76 +23,54 @@ SHT3X         sht30(0x45);
 
 bool connectWiFi() {
   int retryCounter = 0;
+  WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  //Serial.println("Connecting to WiFi");
   while ( WiFi.status() != WL_CONNECTED) {
+    //Serial.print(".");
     retryCounter++;
-    if (DEBUG) Serial.println("connecting Wifi");
-    //Waiting 10 seconds for connection
-    if (retryCounter >= 20) {
-      if (DEBUG) Serial.println("No WiFi connection. Will try tommorow");
+    if (retryCounter >= 100) {
+      //Serial.println("fail");
       return false;
     }
-    delay(500);
+    delay(100);
   }
+  //Serial.println("done");
   return true;    
 }
 
-bool connectMQTT() {
-  int retryCounter = 0;
-    while ( !mqtt.connected() ) {
-      if (DEBUG) Serial.println("connecting MQTT");
-      if ( !mqtt.connect(MQTT_CLIENT_NAME, MQTT_USER, MQTT_PASSWORD) ) {
-        retryCounter++;
-        //Trying to connect 3 times
-        if (retryCounter > 3) {
-          if (DEBUG) Serial.println("No MQTT connection. Will try tommorow");
-          return false;
-        }
-        delay(500);
-      }
-    }
-    return true;
-}
-
 void setup() {
+  //Serial.begin(9600);
+  //Serial.setDebugOutput(true);
+  //Serial.println("Wake Up!");
   pinMode(D0, WAKEUP_PULLUP);
-  if (DEBUG) Serial.begin(9600);
-
   if (!connectWiFi()) {
     goToSleep();  
   } else {
-  
-    sht30.get();
-    float t = sht30.cTemp;
-    float h = sht30.humidity;
-  
-    if (DEBUG) {
-      Serial.print("temp: ");
-      Serial.print(t);
-      Serial.print(" | humid: ");
-      Serial.println(h);
-    }
-  
-    if (!connectMQTT()) {
+    //Serial.print("Connecting to MQTT server...");
+    if (!mqtt.connect(MQTT_CLIENT_NAME, MQTT_USER, MQTT_PASSWORD)) {
+      //Serial.println("fail");
       goToSleep();  
     } else {
-  
+      //Serial.println("done");
+      sht30.get();
+      float t = sht30.cTemp;
+      float h = sht30.humidity;
+      //Serial.println("Sending data and waiting 500ms...");
       mqtt.publish(MQTT_TOPIC_TEMP,  String(t).c_str(), true);
       mqtt.publish(MQTT_TOPIC_HUMID, String(h).c_str(), true);
-      delay(3000);
-      if (DEBUG) {
-        Serial.print("sleeping ");
-        Serial.print(PUBLISH_RATE);
-        Serial.print(" s");
-      }
+      delay(500);
       goToSleep();
     }
   }
 }
 
 void goToSleep() {
-  if (mqtt.connected()) mqtt.disconnect();
-  if (WiFi.status() == WL_CONNECTED) WiFi.disconnect();
+  //Serial.println("Closing all connections...");
+  mqtt.disconnect();
+  wifi.stop();
+  WiFi.disconnect();
+  //Serial.println("Slip.");
   ESP.deepSleep(PUBLISH_RATE * 1e6);  
 }
 
